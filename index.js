65a7f94536526e49a0a48df05e7cfe0e91af30a0
marcus featherston <marcusovfeatherston@gmail.com>
2022-12-26 13:28:58 -0700

    commit tree 28775a8218ed46bfa892d363049464cec85aa6bc

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me! `);
+// }
+
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(giftsToWrap) {
+  let i = 0;
+  while (i < giftsToWrap.length) {
+    console.log(`Wrapped ${giftsToWrap[i]} and added a bow!`);
+    i++;
+  }
+  return giftsToWrap;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, reason) {
+  const messages = [];
+  for (let name = 0; name < names.length; name++) {
+    messages.push(`Thank you, ${names[name]}, for the wonderful ${reason} gift!`)
+  }
+
+  return messages;
+}
+
+
+// writeCards(["Brooke", "Marcus", "Arleigh"], "birthday")
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
